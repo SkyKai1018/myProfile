1df8f3530304b89f48cbd7c0ff0f4a6d92873eb2
SkyKai1018 <rgnkud@gmail.com>
2023-12-18 13:32:36 +0800

    (修改)更新履歷其他的內容

--- src/component/Other.js
+++ src/component/Other.js
@@ -2,10 +2,16 @@ function Other() {
     return (
         <div className="Other mb-3" >
             <div className="row">
-                <div className="Other-col col-md-10 offset-md-1">
-                    <h1>Other</h1>
-                    <p>健行筆記優選</p>
-                    <p>破萬觀看影片</p>
+                <div className="Other-col col-md-10 offset-md-1 px-4 py-4 mb-3">
+                    <h1>其他</h1>
+                    <u>
+                        <li>
+                            <a href="https://www.youtube.com/@SkyKai1018/featured">健行筆記樟之細路徵文活動-優選</a>
+                        </li>
+                        <li>
+                            <a href="https://www.youtube.com/@SkyKai1018/featured">達成youtube 1000訂閱及100,000總觀看數</a>
+                        </li>
+                    </u>
                 </div>
             </div>
         </div>
