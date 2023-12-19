a0278cd9aca687f9696a251dcbc49dadf1ac1b46
SkyKai1018 <rgnkud@gmail.com>
2023-12-19 10:13:11 +0800

    （修改）改用RouteProvider 的方式

--- src/App.js
+++ src/App.js
@@ -1,50 +1,13 @@
 import './App.css';
 import Header from './component/Header.js';
-import Introduction from './component/Introduction.js';
-import WorkExperience from './component/WorkExperience';
-import Education from './component/Education';
-import Skills from './component/Skills';
-import Other from './component/Other';
-import Project from './component/Project';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Blog from './Blog.js';
-import Article1 from './component/posts/1.js'
-
-
-
-function Profile() {
-  return (
-    <div>
-      <div className="Profile">
-        <Introduction />
-        <WorkExperience />
-        <Project />
-        <Education />
-        <Skills />
-        <Other />
-      </div >
-    </div>
-  );
-}
-
-function NoPage() {
-  return <h1>Page not found - 404!!!</h1>;
-}
-
-
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
-      <Header/>
-      <Routes>
-        <Route path="/myprofile" element={<Profile />} />
-        <Route path="/myprofile/1" element={<Article1 />} />
-        <Route path="/myprofile/blog" element={<Blog />} />
-        <Route path="/myprofile/blog/:id" element={<Blog />} />
-        <Route path="/myprofile/*" element={<NoPage />} />
-      </Routes>
-    </Router>
+    <>
+      <Header />
+      <Outlet />
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Blog from './Blog.js';
+import Introduction from './component/Introduction.js';
+import WorkExperience from './component/WorkExperience';
+import Education from './component/Education';
+import Skills from './component/Skills';
+import Other from './component/Other';
+import Project from './component/Project';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+function Profile() {
+  return (
+    <div>
+      <div className="Profile">
+        <Introduction />
+        <WorkExperience />
+        <Project />
+        <Education />
+        <Skills />
+        <Other />
+      </div >
+    </div>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    path: '/myProfile/',
+    element: <App />,
+    children: [
+      {
+        path: '/myProfile/',
+        element: <Profile />,
+      },
+      {
+        path: '/myProfile/blog',
+        element: <Blog />,
+      },
+      {
+        path: '/myProfile/blog/:id',
+        element: <Blog />,
+      },
+    ],
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>
 );
