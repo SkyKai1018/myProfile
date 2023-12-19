ef65215100d286b5032d7f25f516d42a60e367f3
SkyKai1018 <rgnkud@gmail.com>
2023-12-19 11:08:52 +0800

    (修正)測試404.html 的修正方式

---
+++ build/2.js
@@ -0,0 +1,84 @@
+import React from "react"; const Article2 = () => (
+    <>
+        <ul>
+            <li>[ ] Internet<ul>
+                <li>[ ] Internetwork</li>
+                <li>[ ] HTTP</li>
+                <li>[ ] DNS</li>
+                <li>[ ] Domain Name</li>
+                <li>[ ] Hosting</li>
+            </ul>
+            </li>
+            <li>[ ] Language<ul>
+                <li>[ ] JavaScript</li>
+                <li>[ ] Pythone</li>
+            </ul>
+            </li>
+            <li>[ ] Git<ul>
+                <li>[ ] Github</li>
+                <li>[ ] GitLab</li>
+                <li>[ ] Git</li>
+            </ul>
+            </li>
+            <li>[ ] OS and General Knowledge</li>
+            <li>[ ] Relational Databases<ul>
+                <li>[ ] PostgreSQL</li>
+                <li>[ ] MySQL</li>
+            </ul>
+            </li>
+            <li>[ ] NoSQL Databases<ul>
+                <li>[ ] MongoDB</li>
+                <li>[ ] Firebase</li>
+                <li>[ ] Redis</li>
+                <li>[ ] InfluxDB</li>
+                <li>[ ] Cassandra</li>
+            </ul>
+            </li>
+            <li>[ ] APIs<ul>
+                <li>[ ] REST</li>
+                <li>[ ] JSON APIs</li>
+                <li>[ ] gRPC</li>
+                <li>[ ] GraphQL</li>
+                <li>[ ] Autherntication<ul>
+                    <li>[ ] OAuth</li>
+                    <li>[ ] Basic Auth</li>
+                    <li>[ ] Token Auth</li>
+                    <li>[ ] JWT</li>
+                </ul>
+                </li>
+            </ul>
+            </li>
+            <li>[ ] Caching<ul>
+                <li>[ ] Clinet Side</li>
+                <li>[ ] Server Side<ul>
+                    <li>[ ] Redis</li>
+                </ul>
+                </li>
+                <li>[ ] CDN</li>
+            </ul>
+            </li>
+            <li>[ ] Web Security Knowledge<ul>
+                <li>[ ] MD5</li>
+            </ul>
+            </li>
+            <li>[ ] Testing <ul>
+                <li>[ ] Integration</li>
+                <li>[ ] Unit Test</li>
+                <li>[ ] Functional Testing</li>
+            </ul>
+            </li>
+            <li>[ ] CI/CD</li>
+        </ul>
+        <p>職務需求
+            資料庫 ：MySQL ＭongoDB SQL
+            程式語言 ：javascript python js
+            測試 ：Integration Unit Test
+            API : REST GraphQL
+            雲端服務 : AWS (VPC, EC2, S3, SQS, SNS, Lambda, Kinesis, API Gateway, EKS, etc.).
+            其他：docker, k8s, ci/cd,  Multi-Threaded Programming ,Race Condition and Deadlock
+            HTTP caching, nginx</p>
+
+    </>
+);
+
+export default Article2;
